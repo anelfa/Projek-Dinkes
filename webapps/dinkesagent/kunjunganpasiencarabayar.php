ba797f523ae0f4f8eac2592b354d9e3cb59b8907
root <root@localhost.localdomain>
2017-07-27 10:38:38 -0400

    Penambahan Cetak Ulang Tracer

--- webapps/dinkesagent/kunjunganpasiencarabayar.php
+++ webapps/dinkesagent/kunjunganpasiencarabayar.php
@@ -1,46 +1,118 @@
-
-<meta http-equiv="refresh" content="60">
-								
-<script type="text/JavaScript">
-	function AutoRefresh( t ) {
-		setTimeout("location.reload(true);", t);
-	}
-</script>
-
 <?php
-/*] CREATE BY   	: DIDIK ANANG NUGROHO
-[*] DATE(DMY)   	: 23-05-2017 19:24:02
-[*] COMPANY     	: RUMAH SAKIT UMUM DAERAH KOJA
-[*] CITY        	: NORTH JAKARTA, INDONESIA
-[*/
-include "dfconnectionrs.php"; 
-$query ="
-				select
-							KodeRS as koders,
-							Tanggal as tanggal,
-							NamaInstalasi as namainstalasi,
-							CaraBayar as carabayar,
-							JumlahPasien as jumlahpasien,
-							UpdateDate as updatedate
-				from      dbo.[dinkes_kunjunganpasiencarabayar_dayly]
-			";
+ include '../conf/conf.php';
+   /* header("Content-type: application/x-msdownload");
+    header("Content-Disposition: attachment; filename=LaporanRekapKunjuangnJenisBayar.xls");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+    print "$header\n$data";*/
+?>
+<html>
+    <body>
 
-$data = array();
-$zz = sqlsrv_query($conn, $query); 
-while ($allRow = sqlsrv_fetch_array($zz, SQLSRV_FETCH_ASSOC)){ 
-            $datas[] = $allRow;
+    <?php
+    reportsqlinjection();      
+       $tanggal1      = '2017-07-01'; 
+        $tanggal2      = '2017-07-28'; 
+		date_default_timezone_set("Asia/Jakarta");
+		$datatime=date("Y-m-d H:i:s");
 
-        }
-echo "Service Informasi Kunjungan Pasien Berdasarkan Cara Bayar";
-echo '<br>';
-echo '<br>';
-echo json_encode($datas);
-
-$myvars=json_encode($datas);
+      
+		
+		$_poli="SELECT * FROM poliklinik";
+		$poli=bukaquery($_poli);
+		
+		
+		
+		$ppkkode="SELECT * FROM setting";
+		$ppk_kode=bukaquery($ppkkode);
+		
+		 while($ppk = mysql_fetch_array($ppk_kode)) { 
+			$k_ppk=$ppk['kode_ppk'];
+			$n_ppk=$ppk['nama_ppk'];
+		 }
+        
+		//echo $n_ppk;
+		$_jenis = Array('UMUM','BPJS');
+		$_instalasi = Array('Instalasi Rawat Jalan','Instalasi Gawat Darurat','Instalasi Rawat Inap');
+        $myvars="";
+		
+    foreach($_instalasi as $instansi) {
+		
+		if('Instalasi Gawat Darurat'==$instansi)
+		{
+			foreach($_jenis as $jns_byr)
+		{
+			$_sql = "SELECT Count(reg_periksa.kd_pj) AS jumlah_pasien
+				 FROM reg_periksa JOIN penjab ON reg_periksa.kd_pj = penjab.kd_pj WHERE reg_periksa.kd_poli = 'IGD' AND png_jawab='$jns_byr' AND tgl_registrasi  BETWEEN '$tanggal1' AND '$tanggal2'";            
+        $hasil=bukaquery($_sql);
+        while($baris = mysql_fetch_array($hasil)) {
+				$jml_pasien=$baris['jumlah_pasien'];
+		}
+		
+		$post[] = array(
+	
+					'koders'=>$k_ppk,
+					'tanggal'=>$tanggal2,
+					'namainstalasi'=>$instansi,
+					'carabayar'=>$jns_byr,
+					'jumlahpasien'=>$jml_pasien,
+					'updatedate'=>$datatime,
+				);
+		}}
+		else if('Instalasi Rawat Jalan'==$instansi)
+		{
+			foreach($_jenis as $jns_byr)
+		{
+			$_sql = "SELECT Count(reg_periksa.kd_pj) AS jumlah_pasien
+				 FROM reg_periksa JOIN penjab ON reg_periksa.kd_pj = penjab.kd_pj WHERE reg_periksa.kd_poli != 'IGD' AND png_jawab='$jns_byr' AND tgl_registrasi  BETWEEN '$tanggal1' AND '$tanggal2'";            
+        $hasil=bukaquery($_sql);
+		while($baris = mysql_fetch_array($hasil)) {
+				$jml_pasien=$baris['jumlah_pasien'];
+		}
+		
+			$post[] = array(
+	
+					'koders'=>$k_ppk,
+					'tanggal'=>$tanggal2,
+					'namainstalasi'=>$instansi,
+					'carabayar'=>$jns_byr,
+					'jumlahpasien'=>$jml_pasien,
+					'updatedate'=>$datatime,
+				);
+		}}
+		else
+		{
+			foreach($_jenis as $jns_byr)
+		{
+			$_sql = "SELECT Count(reg_periksa.kd_pj) AS jumlah_pasien
+				 FROM reg_periksa JOIN penjab ON reg_periksa.kd_pj = penjab.kd_pj WHERE  png_jawab='$jns_byr' AND status_lanjut='Ranap' AND tgl_registrasi  BETWEEN '$tanggal1' AND '$tanggal2' ";            
+        $hasil=bukaquery($_sql);
+		while($baris = mysql_fetch_array($hasil)) {
+				$jml_pasien=$baris['jumlah_pasien'];
+		}
+			$post[] = array(
+	
+					'koders'=>$k_ppk,
+					'tanggal'=>$tanggal2,
+					'namainstalasi'=>$instansi,
+					'carabayar'=>$jns_byr,
+					'jumlahpasien'=>$jml_pasien,
+					'updatedate'=>$datatime,
+				);
+		}}
+				
+       
+		}
+		echo json_encode($post);
+		$myvars=json_encode($post);
+		
+		
+		
 
 
 ?>
-<input id = 'content' type='hidden' name='content' value='<?php echo $myvars; ?>'>
+
+<input id = 'content' type='text' name='content' value='<?php echo $myvars; ?>'>
 <div id="result"></div>
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
@@ -60,7 +132,7 @@ $myvars=json_encode($datas);
 	});
 </script>
 	
-	
 <!-- <button id="submit" name="submit" type="submit">Send</buttton> -->
 	
-	
+    </body>
+</html>
--- webapps/dinkesagent/kunjunganpasienjeniskelamin.php
+++ webapps/dinkesagent/kunjunganpasienjeniskelamin.php
@@ -1,53 +1,125 @@
-
-<meta http-equiv="refresh" content="60">
-								
-<script type="text/JavaScript">
-	function AutoRefresh( t ) {
-		setTimeout("location.reload(true);", t);
-	}
-</script>
-
 <?php
-/*] CREATE BY   	: DIDIK ANANG NUGROHO
-[*] DATE(DMY)   	: 23-05-2017 19:24:02
-[*] COMPANY     	: RUMAH SAKIT UMUM DAERAH KOJA
-[*] CITY        	: NORTH JAKARTA, INDONESIA
-[*/
-include "dfconnectionrs.php";
-$query ="
-			select	
-						KodeRS as koders,
-						Tanggal as tanggal,
-						NamaInstalasi as namainstalasi,
-						JenisKelamin as jeniskelamin,
-						JumlahPasien as jumlahpasien,
-						UpdateDate as updatedate
-			from      dbo.[dinkes_kunjunganpasienjeniskelamin_dayly]
-			";
+ include '../conf/conf.php';
+   /* header("Content-type: application/x-msdownload");
+    header("Content-Disposition: attachment; filename=LaporanRekapKunjuangnJenisBayar.xls");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+    print "$header\n$data";*/
+?>
+<html>
+    <body>
 
-$data = array();
-$zz = sqlsrv_query($conn, $query); 
-while ($allRow = sqlsrv_fetch_array($zz, SQLSRV_FETCH_ASSOC)){ 
-            $datas[] = $allRow;
+    <?php
+    reportsqlinjection();      
+       $tanggal1      = '2017-07-01'; 
+        $tanggal2      = '2017-07-28'; 
+		date_default_timezone_set("Asia/Jakarta");
+		$datatime=date("Y-m-d H:i:s");
 
-        }
-echo "Service Informasi Kunjungan Pasien Berdasarkan Jenis Kelamin";
-echo '<br>';
-echo '<br>';
-echo json_encode($datas);
-
-$myvars=json_encode($datas);
+      
+		
+		$_poli="SELECT * FROM poliklinik";
+		$poli=bukaquery($_poli);
+		
+		
+		
+		$ppkkode="SELECT * FROM setting";
+		$ppk_kode=bukaquery($ppkkode);
+		
+		 while($ppk = mysql_fetch_array($ppk_kode)) { 
+			$k_ppk=$ppk['kode_ppk'];
+			$n_ppk=$ppk['nama_ppk'];
+		 }
+        
+		//echo $n_ppk;
+		$_jenis = Array('L','P');
+		$_instalasi = Array('Instalasi Rawat Jalan','Instalasi Gawat Darurat','Instalasi Rawat Inap');
+        $myvars="";
+		
+    foreach($_instalasi as $instansi) {
+		
+		if('Instalasi Gawat Darurat'==$instansi)
+		{
+			foreach($_jenis as $jns_kelamin)
+		{
+			$_sql = "SELECT Count(pasien.jk) AS jumlah_pasien
+				 FROM reg_periksa JOIN penjab ON reg_periksa.kd_pj = penjab.kd_pj JOIN pasien ON reg_periksa.no_rkm_medis = pasien.no_rkm_medis WHERE reg_periksa.kd_poli = 'IGD' AND jk='$jns_kelamin' AND tgl_registrasi  BETWEEN '$tanggal1' AND '$tanggal2'";            
+        $hasil=bukaquery($_sql);
+        while($baris = mysql_fetch_array($hasil)) {
+				$jml_pasien=$baris['jumlah_pasien'];
+		}
+		
+		$post[] = array(
+	
+					'koders'=>$k_ppk,
+					'tanggal'=>$tanggal2,
+					'namainstalasi'=>$instansi,
+					'jeniskelamin'=>$jns_kelamin,
+					'jumlahpasien'=>$jml_pasien,
+					'updatedate'=>$datatime,
+				);
+		}}
+		else if('Instalasi Rawat Jalan'==$instansi)
+		{
+			foreach($_jenis as $jns_kelamin)
+		{
+			$_sql = "SELECT Count(pasien.jk) AS jumlah_pasien
+				 FROM reg_periksa JOIN penjab ON reg_periksa.kd_pj = penjab.kd_pj JOIN pasien ON reg_periksa.no_rkm_medis = pasien.no_rkm_medis WHERE reg_periksa.kd_poli != 'IGD' AND jk='$jns_kelamin' AND tgl_registrasi  BETWEEN '$tanggal1' AND '$tanggal2'";            
+        $hasil=bukaquery($_sql);
+		while($baris = mysql_fetch_array($hasil)) {
+				$jml_pasien=$baris['jumlah_pasien'];
+		}
+		
+			$post[] = array(
+	
+					'koders'=>$k_ppk,
+					'tanggal'=>$tanggal2,
+					'namainstalasi'=>$instansi,
+					'jeniskelamin'=>$jns_kelamin,
+					'jumlahpasien'=>$jml_pasien,
+					'updatedate'=>$datatime,
+				);
+		}}
+		else
+		{
+			foreach($_jenis as $jns_kelamin)
+		{
+			$_sql = "SELECT Count(pasien.jk) AS jumlah_pasien
+				 FROM reg_periksa JOIN penjab ON reg_periksa.kd_pj = penjab.kd_pj  JOIN pasien ON reg_periksa.no_rkm_medis = pasien.no_rkm_medis  WHERE  jk='$jns_kelamin' AND status_lanjut='Ranap' AND tgl_registrasi  BETWEEN '$tanggal1' AND '$tanggal2' ";            
+        $hasil=bukaquery($_sql);
+		while($baris = mysql_fetch_array($hasil)) {
+				$jml_pasien=$baris['jumlah_pasien'];
+		}
+			$post[] = array(
+	
+					'koders'=>$k_ppk,
+					'tanggal'=>$tanggal2,
+					'namainstalasi'=>$instansi,
+					'jeniskelamin'=>$jns_kelamin,
+					'jumlahpasien'=>$jml_pasien,
+					'updatedate'=>$datatime,
+				);
+		}}
+				
+       
+		}
+		echo json_encode($post);
+		$myvars=json_encode($post);
+		
+		
+		
 
 
 ?>
-<input id = 'content' type='hidden' name='content' value='<?php echo $myvars; ?>'>
+
+<input id = 'content' type='text' name='content' value='<?php echo $myvars; ?>'>
 <div id="result"></div>
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <script type="text/javascript">
 	$(document).ready(function() {
 	    $.ajax({
-	        url: "http://202.147.199.11/wsdashboarddinkes/wskunjunganpasienjeniskelamin.php",
+	     url: "http://202.147.199.11/wsdashboarddinkes/wskunjunganpasienjeniskelamin.php",
 	        type: "POST",
 	        data: {
 				data: $("#content").val()
@@ -60,7 +132,7 @@ $myvars=json_encode($datas);
 	});
 </script>
 	
-	
 <!-- <button id="submit" name="submit" type="submit">Send</buttton> -->
 	
-	
+    </body>
+</html>
